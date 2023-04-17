7385792ddd816c7558374f2a7e44684a99330c84
SujalGupta52 <67575635+SujalGupta52@users.noreply.github.com>
2023-04-17 22:12:44 +0530

    Edit script.js to create program logic

---
+++ script.js
@@ -0,0 +1,78 @@
+function intialiseButtons() {
+    const buttons = document.querySelectorAll('button');
+    console.log(buttons);
+    buttons.forEach((button) => {
+        button.addEventListener('click',(button) => {
+            console.log(button);
+            processButton(button);
+        });
+    });
+}
+
+function operate(calculationArray) {
+    if(calculationArray[1] === '+')
+        return (+calculationArray[0] + +calculationArray[2]).toFixed(10);
+    if(calculationArray[1] === '-')
+        return (+calculationArray[0] - +calculationArray[2]).toFixed(10);
+    if(calculationArray[1] === '*')
+        return (+calculationArray[0] * +calculationArray[2]).toFixed(10);
+    if(calculationArray[1] === '/') {
+        if(calculationArray[2] === '0')
+            return 'ERROR';
+        else
+            return (+calculationArray[0] / +calculationArray[2]).toFixed(10); 
+    }
+}
+
+function clearCalculationArray() {
+    calculationArray = ['','',''];
+}
+
+function processButton(button) { 
+    if(button.srcElement.classList.value === 'number' && calculationArray[1] === '' && calculationArray[2] === '') {
+        calculationArray[0] += button.srcElement.textContent;
+        document.querySelector('.text').textContent = calculationArray[0];
+    }
+
+    if(button.srcElement.classList.value === 'number' && calculationArray[0] !== '' && calculationArray[1] !== '') {
+        calculationArray[2] += button.srcElement.textContent;
+        document.querySelector('.text').textContent = calculationArray[2];
+    }
+
+    if(button.srcElement.classList.value === 'utility' && calculationArray[0] !== '' && calculationArray[2] === '') {
+        calculationArray[1] += button.srcElement.textContent;
+    }
+
+    if(button.srcElement.classList.value === 'equal' && calculationArray[0] !== '' && calculationArray[1] !== '' && calculationArray [2] !== '') {
+        document.querySelector('.text').textContent = operate(calculationArray);
+        clearCalculationArray();
+    }
+
+    if(button.srcElement.classList.value === 'utility' && calculationArray[0] !== '' && calculationArray[1] !== '' && calculationArray [2] !== '') {
+        console.log(calculationArray);
+        document.querySelector('.text').textContent = operate(calculationArray);
+        calculationArray[0] = operate(calculationArray);
+        calculationArray[1] = button.srcElement.textContent;
+        calculationArray[2] = '';
+        console.log(calculationArray);
+    }
+
+    if(button.srcElement.classList.value === 'clear')
+    {
+        clearCalculationArray();
+        document.querySelector('.text').textContent = '0000000';
+    }
+
+    if(button.srcElement.classList.value === 'delete' && calculationArray[1] === '' && calculationArray[2] === '') {
+        calculationArray[0] = (`${calculationArray[0]}`).substring(0,(`${calculationArray[0]}`).length-1);
+        document.querySelector('.text').textContent = calculationArray[0];
+    }
+
+    if(button.srcElement.classList.value === 'delete' && calculationArray[0] !== '' && calculationArray[1] !== '') {
+        calculationArray[2] = (`${calculationArray[2]}`).substring(0,(`${calculationArray[2]}`).length-1);
+        document.querySelector('.text').textContent = calculationArray[2];
+    }
+}
+
+intialiseButtons();
+let calculationArray = ['','',''];
